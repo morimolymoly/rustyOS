83d3c73f64cc15df9eaa0e726f92344e68d63216
morimolymoly <morimolymoly@gmail.com>
2020-11-16 23:41:12 +0900

    update .cargo/config.toml

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 extern crate rlibc;
@@ -10,7 +11,11 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    //panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop{}
 }
 
@@ -18,6 +23,13 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop{}
+}
+
+#[test_case]
+fn trivial_assert() {
+    print!("trivial assert...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
 
 #[cfg(test)]
