ff39e5677f7fc6d98224f9b70f8916057753b147
morimolymoly <morimolymoly@gmail.com>
2020-11-16 23:24:24 +0900

    ut

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 extern crate rlibc;
@@ -16,4 +18,12 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop{}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
