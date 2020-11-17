375e875de5036fcea7b42fba4cd52f0d737f56e8
morimolymoly <morimolymoly@gmail.com>
2020-11-18 00:23:01 +0900

    imp qemu exit

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,22 @@ fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[test_case]
 fn trivial_assert() {
     print!("trivial assert...");
@@ -38,4 +54,5 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
