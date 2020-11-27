cffc87c5974cf8c046895d4cdd94e02bc570bc0f
morimolymoly <morimolymoly@gmail.com>
2020-11-27 19:42:04 +0900

    int3 trap w/IDT setups

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,19 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 extern crate rlibc;
+
+pub fn init() {
+    interrupts::init_idt();
+}
+
 
 pub trait Testable {
     fn run(&self) -> ();
